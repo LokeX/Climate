ba06ea22cf96ed74e59ba1177263a4b377091416
LokeX <pernielsen117@gmail.com>
2025-04-13 21:35:21 +0200

    optimized nino code

--- nino.nim
+++ nino.nim
@@ -1,14 +1,12 @@
-from algorithm import reverse
 from sequtils import zip
 import strutils
-import htmlhead
 
 type Designation = enum neutral,laNina,elNino
 
-func ninoSignal(value:float):int =
+template ninoSignal(value:float):int =
   if value >= 0.5: 1 elif value <= -0.5: -1 else: 0
 
-func carrySignal(oldSignal,newSignal:int):int =
+template carrySignal(oldSignal,newSignal:int):int =
   if newSignal == 0 or oldSignal*newSignal < 0: 0 else: oldSignal+newSignal
 
 func ninoSignals(values:openArray[float]):seq[int] =
@@ -16,27 +14,28 @@ func ninoSignals(values:openArray[float]):seq[int] =
   for value in values[1..values.high]: 
     result.add carrySignal(result[^1],value.ninoSignal)
 
-iterator reversed[T](x:openArray[T]):T =
+iterator reversed[T](x:openArray[T]):(int,T) =
   var idx = x.high
   while idx >= x.low:
-    yield x[idx]
+    yield (idx,x[idx])
     dec idx
 
 func ninoDesignations(signals:openArray[int]):seq[Designation] =
-  # debugEcho signals
-  for signal in signals.reversed: 
-    # debugEcho signal
-    # debugEcho result
-    case signal:
-      of 0: result.add neutral
-      of int.low..(-5): result.add laNina
-      of 5..int.high: result.add elNino
-      else: result.add if result.len > 0: result[^1] else: neutral
-  reverse result
+  result.setLen signals.len
+  for i,signal in signals.reversed: 
+    result[i] = 
+      if signal <= -5: 
+        laNina
+      elif signal >= 5: 
+        elNino
+      elif signal != 0 and i < signals.high: 
+        result[i+1] 
+      else: 
+        neutral
 
-func parse(fileLines:openArray[string]):(string,seq[string],seq[float]) =
-  result[0] = fileLines[0]&"\n"&fileLines[1]
-  for line in fileLines[2..fileLines.high]:
+func parse(lines:openArray[string]):(string,seq[string],seq[float]) =
+  result[0] = lines[0]&"\n"&lines[1]
+  for line in lines[2..lines.high]:
     result[1].add line[0..3]
     for valStr in line[4..line.high].splitWhitespace: 
       result[2].add valStr.parseFloat
@@ -54,28 +53,53 @@ let
 #Importing the terminal module breaks vs-code intellisense(- WTF?); so we delay to here
 from terminal import ForegroundColor,styledWrite
 
-func fgColor(designation:Designation):ForegroundColor =
+template fgColor(designation:Designation):ForegroundColor =
   case designation:
     of elNino: fgRed
     of laNina: fgBlue
     of neutral: fgWhite
 
-func htmlColor(designation:Designation):string =
+template htmlColor(designation:Designation):string =
   case designation:
     of elNino: "style=\"color:red;\""
-    of laNina: "style=\"color:blue;\""
+    of laNina: "style=\"color:dodgerblue;\""
     of neutral: "style=\"color:green;\""
 
-template headerRow(cells:seq[string]):string =
-  var result:string
+func headerRow(cells:seq[string]):string =
   result.add "\t\t\t<tr>\n"
   for i,cell in cells:
     if i == 0: result.add "\t\t\t\t<th>&nbsp</th>\n"
     result.add "\t\t\t\t<th>"&cell&"</th>\n"
   result.add "\t\t\t</tr>\n"
-  result
 
-var
+const 
+  startHTML = """
+<!DOCTYPE html>
+<html>
+  <head>
+		<meta charset="utf8">
+		<meta name="viewport" content="width=device-width">
+		<style type="text/css">
+			body {
+				background-color: #1B0C0C;
+			}
+			td {
+				text-align:right;
+			}
+			th {
+				width:7%;
+				color:white;
+			}
+		</style>
+  </head>
+  <body>
+"""
+  endHTML = """
+  </body>
+</html>
+"""
+
+let 
   htmlFile = open("nino.html",fmWrite)
 
 htmlFile.write startHTML
@@ -94,3 +118,6 @@ for indexYear,year in years:
 htmlFile.write "\t\t</table>\n"
 htmlFile.write endHTML
 htmlFile.close
+echo "\n\nType:"
+echo "nino.html"
+echo "- to view in browser"
